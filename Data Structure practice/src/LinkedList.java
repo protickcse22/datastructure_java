4f6f32a96df71759e5c4fee8227bdc9b69836412
protick <kprotick@softbdltd.com>
2019-03-18 14:50:58 +0600

    update linkedlist class add more functionality

--- Data Structure practice/src/LinkedList.java
+++ Data Structure practice/src/LinkedList.java
@@ -1,6 +1,10 @@
+// utility class for Linkedlist
 public class LinkedList<T> {
+	// Declaring Head node
 	Node<T> head;
+	int size = 0;
 
+	// utility class for Node
 	static class Node<T> {
 		T data;
 		Node<T> next;
@@ -14,7 +18,7 @@ public class LinkedList<T> {
 	public LinkedList<T> insertNode(LinkedList<T> list, T data) {
 		// creating a new Node
 		Node<T> newNode = new Node<T>(data);
-		newNode.next = null;
+		// newNode.next = null;
 		// Insert node at the beginning of the list
 		if (list.head == null) {
 			list.head = newNode;
@@ -32,7 +36,7 @@ public class LinkedList<T> {
 
 		return list;
 	}
-	
+
 	public LinkedList<T> insertNodePos(LinkedList<T> list, T data, int pos) {
 		// creating a new Node
 		Node<T> newNode = new Node<T>(data);
@@ -45,14 +49,13 @@ public class LinkedList<T> {
 		// Insert node at end of the list
 		else {
 			Node<T> temp = list.head;
-			
-			for(int i=0;i<pos-2;i++)
-			{
+
+			for (int i = 0; i < pos - 2; i++) {
 				temp = temp.next;
 			}
 			newNode.next = temp.next;
 			temp.next = newNode;
-			
+
 		}
 
 		return list;
@@ -63,7 +66,76 @@ public class LinkedList<T> {
 		while (temp != null) {
 			System.out.println("Data: " + temp.data);
 			temp = (Node<T>) temp.next;
+			size++;
 		}
+
+	}
+
+
+/*	public void LinkedListSort(LinkedList<T> list) {
+
+		if (size > 1) {
+			for (int i = 0; i < size; i++) {
+				Node<T> current = list.head;
+				Node<T> next = list.head.next;
+				for (int j = 0; j < size - 1; j++) {
+					if (current.data > next.data) {
+						Node<T> temp = current;
+						current = next;
+						next = temp;
+					}
+					current = next;
+					next = next.next;
+				}
+			}
+		}
+
+	}*/
+
+	public void deleteHeadNode(LinkedList<T> list) {
+		list.head = list.head.next;
+	}
+
+	public void deleteLastNode(LinkedList<T> list) {
+		Node<T> prevNode = list.head;
+		Node<T> nextNode = list.head.next;
+		while (nextNode.next != null) {
+			prevNode = nextNode;
+			nextNode = nextNode.next;
+		}
+		prevNode.next = null;
+	}
+
+	public void deleteposNode(LinkedList<T> list, int pos) {
+
+		Node<T> prevNode, nextNode;
+		prevNode = list.head;
+		nextNode = list.head.next;
+
+		for (int i = 0; i < pos - 2; i++) {
+			prevNode = nextNode;
+			nextNode = nextNode.next;
+		}
+		prevNode.next = nextNode.next;
+
+	}
+
+	public LinkedList<T> reverserList(LinkedList<T> list) {
+
+		Node<T> prev, current, next;
+		prev = null;
+		current = list.head;
+		next = null;
+
+		while (current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		list.head = prev;
+
+		return list;
 
 	}
 
@@ -74,6 +146,16 @@ public class LinkedList<T> {
 		list.insertNode(list, "Reza");
 		list.insertNode(list, "Shovon");
 		list.insertNodePos(list, "Hello", 2);
+		list.insertNodePos(list, "Sami", 3);
+		list.insertNodePos(list, "Munna", 2);
+		list.traverseList(list);
+		System.out.println("Size " + list.size);
+		System.out.println("\n\n");
+		// list.deleteLastNode(list);
+		list.deleteposNode(list, 3);
+		list.traverseList(list);
+		list.reverserList(list);
+		System.out.println("\n\n");
 		list.traverseList(list);
 
 	}
