8b884f97b946616afb72f22ef1d1c6e8d7ae439c
protick kumer dey <kprotickju22@gmail.com>
2019-08-22 21:15:52 +0600

    Update BFS_practice.java

--- Data Structure practice/src/BFS_practice.java
+++ Data Structure practice/src/BFS_practice.java
@@ -1,63 +1,182 @@
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+public class BstPractice {
+	Node root;
 
-public class BFS_practice {
+	static class Node {
+		int data;
+		Node left;
+		Node right;
 
-	static class Graph {
-
-		int vertices;
-		LinkedList<Integer> adjlist[];
-
-		public Graph(int v) {
-			this.vertices = v;
-			adjlist = new LinkedList[this.vertices];
-			for (int i = 0; i < v; i++) {
-				adjlist[i] = new LinkedList<>();
-			}
+		public Node(int data) {
+			this.data = data;
+			left = null;
+			right = null;
 		}
 	}
 
-	public static void addEdge(Graph g, int src, int dst) {
-		g.adjlist[src].add(dst);
-		 g.adjlist[dst].add(src);
+	public Node insertData(Node root, int data) {
+		if (root == null) {
+			Node newNode = new Node(data);
+			root = newNode;
+		} else if (data <= root.data) {
+			root.left = insertData(root.left, data);
+		} else {
+			root.right = insertData(root.right, data);
+		}
+
+		return root;
+
 	}
 
-	public static void BFS(Graph g, int src) {
-		boolean[] visited = new boolean[g.vertices];
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.add(src);
-		visited[src] = true;
+	public void display(Node root) {
+		if (root == null)
+			return;
+		Queue<Node> q = new java.util.LinkedList<Node>();
+		q.add(root);
 		while (!q.isEmpty()) {
-			src = q.peek();
-			System.out.println(src + " ");
-			q.remove();
-			Iterator<Integer> itr = g.adjlist[src].listIterator();
-			while (itr.hasNext()) {
-				int n = itr.next();
-				if (!visited[n]) {
-					visited[n] = true;
-					q.add(n);
-				}
-
+			Node temp = q.peek();
+			System.out.println(temp.data);
+			if (temp.left != null) {
+				q.add(temp.left);
 			}
+
+			if (temp.right != null) {
+				q.add(temp.right);
+			}
+
+			q.remove();
+
 		}
 	}
 
-	public static void main(String[] args) {
-		int v = 6;
-		Graph g = new Graph(v);
-		addEdge(g, 0, 1);
-		addEdge(g, 0, 2);
-		addEdge(g, 0, 3);
-		addEdge(g, 0, 4);
-		addEdge(g, 1, 4);
-		addEdge(g, 1, 5);
-		addEdge(g, 2, 3);
-		addEdge(g, 3, 4);
-		addEdge(g, 4, 5);
+	public void inorder(Node root) {
+		if (root == null)
+			return;
+		inorder(root.left);
+		System.out.println(root.data);
+		inorder(root.right);
+	}
 
-		BFS(g, 2);
+	public void preorder(Node root) {
+		if (root == null)
+			return;
+		System.out.println(root.data);
+		preorder(root.left);
+		preorder(root.right);
+
+	}
+
+	public void postorder(Node root) {
+		if (root == null)
+			return;
+		postorder(root.left);
+		postorder(root.right);
+		System.out.println(root.data);
+	}
+
+	public void printChild(Node root) {
+
+		if (root == null)
+			return;
+		printChild(root.left);
+		printChild(root.right);
+		if (root.left == null && root.right == null) {
+			System.out.println(root.data);
+		}
+
+	}
+
+	public int maxValue(Node root) {
+		if (root == null)
+			return -1;
+		Node current = root;
+		while (current.right != null) {
+			current = current.right;
+		}
+		return current.data;
+
+	}
+
+	public int minValue(Node root) {
+		if (root == null)
+			return -1;
+		Node current = root;
+		while (current.left != null) {
+			current = current.left;
+		}
+		return current.data;
+
+	}
+
+	int height(Node root) {
+		if (root == null)
+			return 0;
+		return Math.max(height(root.left), height(root.right)) + 1;
+	}
+
+	// Iterative search
+	public void search(Node root, int data) {
+		Node tempNode = root;
+		while (tempNode != null) {
+			if (tempNode.data == data) {
+				System.out.println("Data found");
+				return;
+			} else if (data > tempNode.data) {
+				tempNode = tempNode.right;
+			} else {
+				tempNode = tempNode.left;
+			}
+		}
+
+		System.out.println("Not found");
+		return;
+
+	}
+
+	public void printKDistant(Node root, int level)  
+    { 
+        if (root == null) 
+            return; 
+        if (level == 0)  
+        { 
+            System.out.print(root.data + " "); 
+            return; 
+        }  
+        else 
+        { 
+            printKDistant(root.left, level - 1); 
+            printKDistant(root.right, level - 1); 
+        } 
+    } 
+
+	public static void main(String... agrs) {
+		BstPractice bst = new BstPractice();
+		bst.root = bst.insertData(bst.root, 50);
+		bst.root = bst.insertData(bst.root, 30);
+		bst.root = bst.insertData(bst.root, 20);
+		bst.root = bst.insertData(bst.root, 40);
+		bst.root = bst.insertData(bst.root, 70);
+		bst.root = bst.insertData(bst.root, 60);
+		bst.root = bst.insertData(bst.root, 80);
+		System.out.println("Level order");
+		bst.display(bst.root);
+		System.out.println("Inorder");
+		bst.inorder(bst.root);
+		System.out.println("Preorder");
+		bst.preorder(bst.root);
+		System.out.println("Postorder");
+		bst.postorder(bst.root);
+		System.out.println("Child node");
+		bst.printChild(bst.root);
+		System.out.println("Max");
+		System.out.println(bst.maxValue(bst.root));
+		System.out.println("Min");
+		System.out.println(bst.minValue(bst.root));
+
+		System.out.println("Height");
+		System.out.println(bst.height(bst.root));
+		System.out.println("For search operation");
+		bst.printKDistant(bst.root, 1);
 
 	}
 
